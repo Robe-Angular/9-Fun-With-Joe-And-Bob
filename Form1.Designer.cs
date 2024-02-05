13399daa05f865c8c9356aa60fa4451064a2bfa6
robe <robe@robe.com>
2024-02-05 17:34:36 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace _3_Working_With_Some_Guys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.joesCash = new System.Windows.Forms.Label();
+            this.bobsCash = new System.Windows.Forms.Label();
+            this.BankCash = new System.Windows.Forms.Label();
+            this.joeGivesToBob = new System.Windows.Forms.Button();
+            this.bobGivesToJoe = new System.Windows.Forms.Button();
+            this.loadJoe = new System.Windows.Forms.Button();
+            this.saveJoe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Give $10 to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Receive $5 from Bob";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // joesCash
+            // 
+            this.joesCash.AutoSize = true;
+            this.joesCash.Location = new System.Drawing.Point(27, 9);
+            this.joesCash.Name = "joesCash";
+            this.joesCash.Size = new System.Drawing.Size(35, 13);
+            this.joesCash.TabIndex = 2;
+            this.joesCash.Text = "label1";
+            this.joesCash.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bobsCash
+            // 
+            this.bobsCash.AutoSize = true;
+            this.bobsCash.Location = new System.Drawing.Point(27, 50);
+            this.bobsCash.Name = "bobsCash";
+            this.bobsCash.Size = new System.Drawing.Size(35, 13);
+            this.bobsCash.TabIndex = 3;
+            this.bobsCash.Text = "label2";
+            // 
+            // BankCash
+            // 
+            this.BankCash.AutoSize = true;
+            this.BankCash.Location = new System.Drawing.Point(27, 99);
+            this.BankCash.Name = "BankCash";
+            this.BankCash.Size = new System.Drawing.Size(35, 13);
+            this.BankCash.TabIndex = 4;
+            this.BankCash.Text = "label3";
+            // 
+            // joeGivesToBob
+            // 
+            this.joeGivesToBob.Location = new System.Drawing.Point(30, 184);
+            this.joeGivesToBob.Name = "joeGivesToBob";
+            this.joeGivesToBob.Size = new System.Drawing.Size(75, 47);
+            this.joeGivesToBob.TabIndex = 5;
+            this.joeGivesToBob.Text = "Joe gives $10 to Bob";
+            this.joeGivesToBob.UseVisualStyleBackColor = true;
+            this.joeGivesToBob.Click += new System.EventHandler(this.joeGivesToBob_Click);
+            // 
+            // bobGivesToJoe
+            // 
+            this.bobGivesToJoe.Location = new System.Drawing.Point(116, 184);
+            this.bobGivesToJoe.Name = "bobGivesToJoe";
+            this.bobGivesToJoe.Size = new System.Drawing.Size(75, 47);
+            this.bobGivesToJoe.TabIndex = 6;
+            this.bobGivesToJoe.Text = "Bob gives $5 to Joe";
+            this.bobGivesToJoe.UseVisualStyleBackColor = true;
+            this.bobGivesToJoe.Click += new System.EventHandler(this.bobGivesToJoe_Click);
+            // 
+            // loadJoe
+            // 
+            this.loadJoe.Location = new System.Drawing.Point(121, 248);
+            this.loadJoe.Name = "loadJoe";
+            this.loadJoe.Size = new System.Drawing.Size(69, 49);
+            this.loadJoe.TabIndex = 7;
+            this.loadJoe.Text = "Load Joe";
+            this.loadJoe.UseVisualStyleBackColor = true;
+            this.loadJoe.Click += new System.EventHandler(this.loadJoe_Click);
+            // 
+            // saveJoe
+            // 
+            this.saveJoe.Location = new System.Drawing.Point(30, 248);
+            this.saveJoe.Name = "saveJoe";
+            this.saveJoe.Size = new System.Drawing.Size(69, 49);
+            this.saveJoe.TabIndex = 8;
+            this.saveJoe.Text = "Save Joe";
+            this.saveJoe.UseVisualStyleBackColor = true;
+            this.saveJoe.Click += new System.EventHandler(this.saveJoe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 332);
+            this.Controls.Add(this.saveJoe);
+            this.Controls.Add(this.loadJoe);
+            this.Controls.Add(this.bobGivesToJoe);
+            this.Controls.Add(this.joeGivesToBob);
+            this.Controls.Add(this.BankCash);
+            this.Controls.Add(this.bobsCash);
+            this.Controls.Add(this.joesCash);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label joesCash;
+        private System.Windows.Forms.Label bobsCash;
+        private System.Windows.Forms.Label BankCash;
+        private System.Windows.Forms.Button joeGivesToBob;
+        private System.Windows.Forms.Button bobGivesToJoe;
+        private System.Windows.Forms.Button loadJoe;
+        private System.Windows.Forms.Button saveJoe;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+
+
+namespace _3_Working_With_Some_Guys
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public void UpdateForm()
+        {
+            joesCash.Text = joe.Name + " has $" + joe.Cash;
+            bobsCash.Text = bob.Name + " has $" + bob.Cash;
+            BankCash.Text = "The bank has $" + bank;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            bob = new Guy()
+            {
+                Cash = 100,
+                Name = "Bob"
+            };
+            
+
+
+            joe = new Guy() { 
+                Cash = 50,
+                Name = "Joe"
+            };
+            
+
+            UpdateForm();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        
+
+        
+
+        private void joeGivesToBob_Click(object sender, EventArgs e)
+        {
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        private void bobGivesToJoe_Click(object sender, EventArgs e)
+        {
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
+        }
+
+        private void saveJoe_Click(object sender, EventArgs e)
+        {
+            using (Stream output = File.Create("Guy_File.dat"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(output, joe);
+            }
+        }
+
+        private void loadJoe_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("Guy_File.dat"))
+            {
+                MessageBox.Show("File doesn't exist", "Not found", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+            using(Stream input = File.OpenRead("Guy_File.dat"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                joe = (Guy)formatter.Deserialize(input);
+            }
+            this.UpdateForm();
+        }
+    }
+}
